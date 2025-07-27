433825450a2b21a3c1a44e6c5d1ae14cfed0b2d1
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-07-27 11:33:01 +0700

    create models ORM

--- 01-backend-nodejs-jwt2/src/config/connectDB.js
+++ 01-backend-nodejs-jwt2/src/config/connectDB.js
@@ -1,10 +1,15 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('jwt2', 'root', null, {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    }
+);
 
 const connection = async () => {
     try {
