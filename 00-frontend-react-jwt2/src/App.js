d77513d8ad7a55ae4deb4d72248b9bc3d278f15f
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-09-12 23:19:09 +0700

    react router dom and install bootstrap 5

--- 00-frontend-react-jwt2/src/App.js
+++ 00-frontend-react-jwt2/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.scss';
+import Login from './components/Login/Login';
+// import Nav from './components/Navigation/Nav';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world React with Hoi Dan IT
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Nav /> */}
+      <div className='app-container'>
+        <Switch>
+          <Route path="/about">
+            abouat
+          </Route>
+          <Route path="/users">
+            usser
+          </Route>
+          <Route path="/contact">
+            contact
+          </Route>
+          <Route path="/home">
+            home
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="*">
+            404 not found
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ 00-frontend-react-jwt2/src/components/Login/Login.js
@@ -0,0 +1,8 @@
+const Login = (props) => {
+    return (
+        <div className="login-container">
+            Login
+        </div>
+    )
+}
+export default Login;
