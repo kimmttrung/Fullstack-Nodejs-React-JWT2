c515054c4695d25eacb685a24ae584ad0cddcaa0
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-09-14 09:58:59 +0700

    design login reponsive

--- 00-frontend-react-jwt2/src/components/Login/Login.js
+++ 00-frontend-react-jwt2/src/components/Login/Login.js
@@ -2,10 +2,10 @@ import "./Login.scss"
 
 const Login = (props) => {
     return (
-        <div className="login-container mt-5">
+        <div className="login-container">
             <div className="container">
-                <div className="row">
-                    <div className="content-left red col-md-7 ">
+                <div className="row px-3 d-sm-0">
+                    <div className="content-left col-12 d-none  col-sm-7 d-sm-block">
                         <div className="brand">
                             Website TrungD
                         </div>
@@ -13,13 +13,20 @@ const Login = (props) => {
                             Nền tảng thương mại điện tử yêu thích ở châu Á, share and connect with people
                         </div>
                     </div>
-                    <div className="content-right green col-md-5 d-flex flex-column gap-3 py-3">
+                    <div className="content-right green col-12 col-sm-5 d-flex flex-column gap-3 py-3">
+                        <div className="brand d-sm-none">
+                            Website TrungD
+                        </div>
                         <input type="text" className="form-control" placeholder="Email address or phone number" />
                         <input type="text" className="form-control" placeholder="Password" />
                         <button className="btn btn-primary">
                             Login
                         </button>
-                        <span className="text-center">Forgot your pasword</span>
+                        <span className="text-center">
+                            <a className="forgot-pasword" href='/about'>
+                                Forgot your pasword?
+                            </a>
+                        </span>
                         <hr />
                         <div className="text-center">
                             <button className="btn btn-success">Create new account</button>
