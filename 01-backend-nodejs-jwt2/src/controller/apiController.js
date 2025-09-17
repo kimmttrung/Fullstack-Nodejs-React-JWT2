9c099c0d1e5a545d1861781dd2169d0c8cda6120
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-09-17 10:11:37 +0700

    API register create user

--- 01-backend-nodejs-jwt2/src/controller/apiController.js
+++ 01-backend-nodejs-jwt2/src/controller/apiController.js
@@ -1,3 +1,5 @@
+import loginRegisterService from '../service/loginRegisterService';
+
 const testAPI = (req, res) => {
     return res.status(200).json({
         message: "ok",
@@ -5,8 +7,37 @@ const testAPI = (req, res) => {
     })
 }
 
-const handleRegister = (req, res) => {
-    console.log("call me", req.body);
+const handleRegister = async (req, res) => {
+    try {
+        if (!req.body.email || !req.body.phone || !req.body.password) {
+            return res.status(200).json({
+                EM: 'Error required parameters',
+                EC: '1',
+                DT: '',
+            })
+        }
+        if (req.body.password && req.body.password.length < 4) {
+            return res.status(200).json({
+                EM: 'Your password must have more than 3 letters',
+                EC: '1',
+                DT: '',
+            })
+        }
+        // srever create user
+        let data = await loginRegisterService.registerNewUser(req.body);
+
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: '',
+        })
+    } catch (error) {
+        return res.status(200).json({
+            EM: "Error from server",
+            EC: '-1',// error code
+            DT: '', // data
+        })
+    }
 }
 module.exports = {
     testAPI, handleRegister
---
+++ 01-backend-nodejs-jwt2/src/service/loginRegisterService.js
@@ -0,0 +1,73 @@
+import db from '../models/index'
+import bcrypt from 'bcryptjs';
+
+
+const saltRounds = 10;
+const hashUserPassword = (userPassword) => {
+    let hashPassword = bcrypt.hashSync(userPassword, saltRounds);
+    return hashPassword;
+}
+
+const checkEmailExist = async (userEmail) => {
+    let user = await db.User.findOne({
+        where: { email: userEmail }
+    })
+    if (user) {
+        return true;
+    }
+    return false;
+}
+const checkPhoneExist = async (userPhone) => {
+    let user = await db.User.findOne({
+        where: { phonenumber: userPhone }
+    })
+    if (user) {
+        return true;
+    }
+    return false;
+}
+
+const registerNewUser = async (rawUserData) => {
+    try {
+        // check email, phonenumber are axist
+        let isEmailExist = await checkEmailExist(rawUserData.email);
+        if (isEmailExist === true) {
+            return {
+                EM: 'The email is alreadly exist',
+                EC: 1
+            }
+        }
+        let isPhoneExist = await checkPhoneExist(rawUserData.phone);
+        if (isPhoneExist === true) {
+            return {
+                EM: 'The phone is alreadly exist',
+                EC: 1
+            }
+        }
+        // hash user password
+        let hashPassword = hashUserPassword(rawUserData.password);
+
+        // create user
+        await db.User.create({
+            email: rawUserData.email,
+            phonenumber: rawUserData.phone,
+            password: rawUserData.hashPassword,
+            username: rawUserData.username
+        })
+
+        return {
+            EM: 'A user is created successfully!',
+            EC: 0
+        }
+    } catch (error) {
+        console.log("check error", error);
+        return {
+            EM: 'Something wrongs in service',
+            EC: -2
+        }
+    }
+}
+
+module.exports = {
+    registerNewUser
+}
