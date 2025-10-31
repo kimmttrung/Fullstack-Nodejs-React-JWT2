db456e3c370646891805a616810b83de7d26c742
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-10-31 23:46:00 +0700

    modal create user part 2 check validate

--- 00-frontend-react-jwt2/src/components/ManageUsers/ModalUser.js
+++ 00-frontend-react-jwt2/src/components/ManageUsers/ModalUser.js
@@ -4,16 +4,30 @@ import Modal from 'react-bootstrap/Modal';
 import { fetchGroup } from '../../service/userService';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import _ from "lodash";
 
 const ModalUser = (props) => {
-    const [email, setEmail] = useState();
-    const [phone, setPhone] = useState();
-    const [username, setUername] = useState();
-    const [password, setPassword] = useState();
-    const [address, setAddress] = useState();
-    const [sex, setSex] = useState();
-    const [group, setGroup] = useState();
+    const defaultUserData = {
+        email: '',
+        phone: '',
+        username: "",
+        password: '',
+        address: '',
+        sex: '',
+        group: ''
+    }
+    const validsInputsDefault = {
+        email: true,
+        phone: true,
+        username: true,
+        password: true,
+        address: true,
+        sex: true,
+        group: true,
+    }
+    const [validInputs, setValidInputs] = useState(validsInputsDefault);
 
+    const [userData, setUserData] = useState(defaultUserData);
     const [userGroup, setUserGroup] = useState([]);
     const getGroups = async () => {
         let res = await fetchGroup();
@@ -26,6 +40,25 @@ const ModalUser = (props) => {
     useEffect(() => {
         getGroups();
     }, [])
+
+    const handleOnChangeInput = (value, name) => {
+        let _userData = _.cloneDeep(userData);
+        _userData[name] = value;
+        setUserData(_userData);
+    }
+
+    const checkValidateInput = () => {
+        // create user
+        let arr = ['email', 'phone', 'password', 'group'];
+        let check = true;
+        for (let i = 0; i < arr.length; i++) {
+            if (!userData[arr[i]]) {
+                toast.error(`Empty input ${arr[i]}`);
+                check = false;
+                break;
+            }
+        }
+    }
     return (
         <>
             <Modal
@@ -44,27 +77,42 @@ const ModalUser = (props) => {
                     <div className='content-body row'>
                         <div className='col-12 col-sm-6 form-group'>
                             <label>Email address (<span className='red'>*</span>)</label>
-                            <input className='form-control' type='email' />
+                            <input
+                                className={validInputs.email ? 'form-control' : 'form-control is-invalid'}
+                                type='email' value={userData.email}
+                                onChange={(event) => handleOnChangeInput(event.target.value, "email")}
+                            />
                         </div>
                         <div className='col-12 col-sm-6 form-group'>
                             <label>Phonenumber (<span className='red'>*</span>)</label>
-                            <input className='form-control' type='text' />
+                            <input className='form-control' type='text' value={userData.phone}
+                                onChange={(event) => handleOnChangeInput(event.target.value, "phone")}
+
+                            />
                         </div>
                         <div className='col-12 col-sm-6 form-group'>
                             <label>Username </label>
-                            <input className='form-control' type='email' />
+                            <input className='form-control' type='email' value={userData.username}
+                                onChange={(event) => handleOnChangeInput(event.target.value, "username")}
+                            />
                         </div>
                         <div className='col-12 col-sm-6 form-group'>
                             <label>Password (<span className='red'>*</span>)</label>
-                            <input className='form-control' type='password' />
+                            <input className='form-control' type='password' value={userData.password}
+                                onChange={(event) => handleOnChangeInput(event.target.value, "password")}
+                            />
                         </div>
                         <div className='col-12 col-sm-12 form-group'>
                             <label>Address </label>
-                            <input className='form-control' type='text' />
+                            <input className='form-control' type='text' value={userData.address}
+                                onChange={(event) => handleOnChangeInput(event.target.value, "address")}
+                            />
                         </div>
                         <div className='col-12 col-sm-6 form-group'>
                             <label>Gender</label>
-                            <select className='form-select'>
+                            <select className='form-select'
+                                onChange={(event) => handleOnChangeInput(event.target.value, "sex")}
+                            >
                                 <option selected value="Male">Male</option>
                                 <option value="Female">Female</option>
                                 <option value="Other">Other</option>
@@ -72,7 +120,10 @@ const ModalUser = (props) => {
                         </div>
                         <div className='col-12 col-sm-6 form-group'>
                             <label>Group (<span className='red'>*</span>)</label>
-                            <select className='form-select'>
+                            <select
+                                className='form-select'
+                                onChange={(event) => handleOnChangeInput(event.target.value, "group")}
+                            >
                                 {userGroup.length > 0 &&
                                     userGroup.map((item, index) => {
                                         return (
